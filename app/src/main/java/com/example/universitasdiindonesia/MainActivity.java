a3f6f52b5dc884d7ba76073b3ce45fff01e1fa33
aulialigar <aulialsh69@gmail.com>
2019-09-10 12:50:34 +0700

    add icon about

--- app/src/main/java/com/example/universitasdiindonesia/MainActivity.java
+++ app/src/main/java/com/example/universitasdiindonesia/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.universitasdiindonesia;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -28,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
         rvUniv.setLayoutManager(new LinearLayoutManager(this));
         ListUnivAdapter listHeroAdapter = new ListUnivAdapter(list);
         rvUniv.setAdapter(listHeroAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void onComposeAction(MenuItem mi) {
+        // pengecekannya di sini
     }
 }
